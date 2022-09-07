5320ee8c1bf41da46369f197f01a25d4299ab98e
ivan-tana <84873112+ivan-tana@users.noreply.github.com>
2022-09-07 02:38:28 +0100

    Create notification.py

---
+++ notification.py
@@ -0,0 +1,10 @@
+from pushbullet import Pushbullet 
+
+def PushNote(title, body,API_KEY):
+    pb = Pushbullet(API_KEY)
+    message = pb.push_note(title, body)
+
+
+def PushLink(title, body, Link,API_KEY):
+    pb = Pushbullet(API_KEY)
+    message = pb.push_link(title, Link, body)
